7f98d5b088c9a50be12514535091fb5268c728b5
Bloodsoul <Bloodsoul@users.noreply.github.com>
2015-09-21 13:45:33 +0200

    Update tools.php

--- tools.php
+++ tools.php
@@ -1,15 +1,28 @@
 <?php
 
+$result;
+
 switch ($_GET["action"])
 {
         case "compare_version":
-                $result = version_compare($_GET["sample"], $_GET["latest"]);
-                break;
-        case "contains_placeholder":
-                $result = "false";
                 if (strpos($_GET["sample"], "x"))
                 {
-                        $result = "true";
+                        if (version_compare(str_replace("x", "0", $_GET["sample"]), $_GET["latest"]) != -1)
+                        {
+                                $result = -1;
+                        }
+                        elseif (version_compare(str_replace("x", "999", $_GET["sample"]), $_GET["latest"]) != 1)
+                        {
+                                $result = 1;
+                        }
+                        else
+                        {
+                                $result = 0;
+                        }
+                }
+                else
+                {
+                        $result = version_compare($_GET["sample"], $_GET["latest"]);
                 }
                 break;
 }
