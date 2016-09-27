6ad117759e29a706a59ce8638e9ae97bb8be7f3e
God Mod <godmod23@web.de>
2016-09-27 17:34:36 +0200

    Nicer Image

--- tools_replacement.php
+++ tools_replacement.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'github.php';
-
+ini_set('display_errors', 1);
 class VersionCheck
 {
     public function handler()
@@ -77,15 +77,27 @@ class VersionCheck
     private function createImageWithText($text)
     {
         $x = 565;
-        $y = 25;
+        $y = 50;
 
         $image = imagecreatetruecolor($x, $y);
-        $background = imagecolorallocate($image, 255, 255, 255);
+        $background = imagecolorallocate($image, 170, 170, 170);
         imagefilledrectangle($image, 0, 0, $x, $y, $background);
+		
+		$background2 = imagecolorallocate($image, 249, 249, 249);
+        imagefilledrectangle($image, 1, 1, $x-2, $y-2, $background2);
 
+		$font = 'fonts/verdana.ttf';
         $color = imagecolorallocate($image, 0, 0, 0);
-        imageString($image, 5, 5, 5, $text, $color);
+        //imageString($image, 5, 5, 5, $text, $color);
+		imagettftext($image, 12, 0, 55, 21, $color, $font, $text);
+		imagettftext($image, 12, 0, 55, 43, $color, $font, "Das ist die zweite Zeile");
+		
+		$myicon = 'icons/clock.png';
+		$icon = imagecreatefrompng($myicon);
+		list($width_orig, $height_orig) = getimagesize($myicon);
 
+		imagecopyresampled($image, $icon, 5, 5, 0, 0, 42, 42, $width_orig, $height_orig);
+		
         return $image;
     }
 
