1fc5b024ff3ea86a25e7ba8cbff0d2eae8df81c1
God Mod <godmod23@web.de>
2016-09-27 18:34:32 +0200

    Added caching, multi lang support, and multiple sublines

--- tools_replacement.php
+++ tools_replacement.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'github.php';
-ini_set('display_errors', 1);
+//ini_set('display_errors', 1);
 class VersionCheck
 {
     public function handler()
@@ -13,9 +13,20 @@ class VersionCheck
                 {
                     break;
                 }
-                $eqdkp = new GitHub('EQdkpPlus', $this->get('repo'));
-                $text = $this->compareVersion($this->get('version'), $eqdkp->getLatestVersion());
-                $this->displayImage($text);
+				
+				$lang = (strlen($this->get('lang'))) ? $this->get('lang') : 'en';
+				
+				$strCacheName = md5($this->get('repo').'_'.$this->get('version').'_'.$lang);
+				$strCacheFolder = 'cache/data/';
+				if(file_exists($strCacheFolder.$strCacheName.'.txt') && (filemtime($strCacheFolder.$strCacheName.'.txt')+3600 > time())){
+					$arrText = unserialize(file_get_contents($strCacheFolder.$strCacheName.'.txt'));
+				} else {
+					$eqdkp = new GitHub('EQdkpPlus', $this->get('repo'));
+					$arrText = $this->compareVersion($this->get('version'), $eqdkp->getLatestVersion(), $lang, $this->get('repo'));
+					file_put_contents($strCacheFolder.$strCacheName.'.txt', serialize($arrText));
+				}
+                
+                $this->displayImage($arrText);
                 break;
         }
     }
@@ -24,13 +35,13 @@ class VersionCheck
     {
         if (isset($_GET[$parameter]) && !empty($_GET[$parameter]))
         {
-            return $_GET[$parameter];
+            return filter_var($_GET[$parameter], FILTER_SANITIZE_STRING);
         }
 
         return;
     }
 
-    private function compareVersion($sample, $latest)
+    private function compareVersion($sample, $latest, $lang='en', $repo='')
     {
         $result;
         $text;
@@ -58,26 +69,60 @@ class VersionCheck
             $result = version_compare($sample, $latest);
         }
 
-        switch ($result)
-        {
-            case 0:
-                $text = 'aktuelle Version: '.$latest;
-                break;
-            case 1:
-                $text = 'kommende Version: '.$sample;
-                break;
-            case -1:
-                $text = 'veraltete Version: '.$sample;
-                break;
-        }
+		if($lang == 'de'){
+			$text = 'Dieser Artikel bezieht sich auf die ';
+			$subtext = "";
+			$icon = "";
+			switch ($result)
+			{
+				case 0:
+					$text .= 'aktuelle Version: '.$latest;
+					break;
+				case 1:
+					$text .= 'kommende Version: '.$sample;
+					$subtext = array('Wir übernehmen keinen Support für Funktionen oder Informationen,', 'die sich auf die zukünftige Version beziehen.');
+					$icon = 'icons/attention.png';
+					break;
+				case -1:
+					$text .= 'veraltete Version: '.$sample;
+					$subtext = array('Bitte hilf mit, die fehlenden Informationen zu recherchieren ', 'und einzufügen.');
+					$icon = 'icons/clock.png';
+					break;
+			}
 
-        return 'Dieser Artikel bezieht sich auf die '.$text;
+		} else {
+			$text = 'This article.... ';
+			$subtext = "";
+			$icon = "";
+			switch ($result)
+			{
+				case 0:
+					$text .= 'aktuelle Version: '.$latest;
+					break;
+				case 1:
+					$text .= 'kommende Version: '.$sample;
+					$subtext = array('Wir übernehmen keinen Support für Funktionen oder Informationen,', 'die sich auf die zukünftige Version beziehen.');
+					$icon = 'icons/attention.png';
+					break;
+				case -1:
+					$text .= 'veraltete Version: '.$sample;
+					$subtext = array('Bitte hilf mit, die fehlenden Informationen zu recherchieren ', 'und einzufügen.');
+					$icon = 'icons/clock.png';
+					break;
+			}
+			
+		}
+
+        return array($result, $text, $subtext, $icon, $lang, $repo);
     }
 
-    private function createImageWithText($text)
+    private function createImageWithText($text, $subtext, $myicon, $result)
     {
         $x = 565;
         $y = 50;
+		
+		$intSubLines = (is_array($subtext)) ? count($subtext) : ((strlen($subtext)) ? 1 : 0);
+		$y = $y -25 + ($intSubLines*25);
 
         $image = imagecreatetruecolor($x, $y);
         $background = imagecolorallocate($image, 170, 170, 170);
@@ -85,30 +130,58 @@ class VersionCheck
 		
 		$background2 = imagecolorallocate($image, 249, 249, 249);
         imagefilledrectangle($image, 1, 1, $x-2, $y-2, $background2);
-
-		$font = 'fonts/verdana.ttf';
-        $color = imagecolorallocate($image, 0, 0, 0);
-        //imageString($image, 5, 5, 5, $text, $color);
-		imagettftext($image, 12, 0, 55, 21, $color, $font, $text);
-		imagettftext($image, 12, 0, 55, 43, $color, $font, "Das ist die zweite Zeile");
 		
-		$myicon = 'icons/clock.png';
-		$icon = imagecreatefrompng($myicon);
-		list($width_orig, $height_orig) = getimagesize($myicon);
+		if($myicon != ""){
+			$icon = imagecreatefrompng($myicon);
+			list($width_orig, $height_orig) = getimagesize($myicon);
 
-		imagecopyresampled($image, $icon, 5, 5, 0, 0, 42, 42, $width_orig, $height_orig);
+			imagecopyresampled($image, $icon, 5, ($y/2 - 21), 0, 0, 42, 42, $width_orig, $height_orig);
+			$imageMarginLeft = 55;
+		} else {
+			$imageMarginLeft = 5;
+		}
+		
+		$font = 'fonts/verdana.ttf';
+		$color = imagecolorallocate($image, 0, 0, 0);
+		imagettftext($image, 11, 0, $imageMarginLeft, 21, $color, $font, $text);
+		
+		if(is_array($subtext)){
+			$intStart = 43;
+			foreach($subtext as $val){
+				imagettftext($image, 11, 0, $imageMarginLeft, $intStart, $color, $font, $val);
+				$intStart += 22;
+			}
+		} elseif(strlen($subtext)){
+			imagettftext($image, 11, 0, $imageMarginLeft, 43, $color, $font, $subtext);
+		}
 		
         return $image;
     }
 
-    private function displayImage($text)
+    private function displayImage($arrText)
     {
-        header ('Content-Type: image/png');
+	    header ('Content-Type: image/png');
+		
+		//show empty pixel if version is recent
+		if($arrText[0] == 0){
+			echo base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=');
+			exit;
+		}
 
-        $image = $this->createImageWithText($text);
+		//Check for cached images
+		$strCacheName = md5($arrText[0].'_'.$arrText[4].'_'.$arrText[5]);
+		$strCacheFolder = 'cache/images/';
+		if(file_exists($strCacheFolder.$strCacheName.'.png') && (filemtime($strCacheFolder.$strCacheName.'.png')+3600 > time())){
+			echo file_get_contents($strCacheFolder.$strCacheName.'.png');
+			exit;
+		} else {
+			$image = $this->createImageWithText($arrText[1], $arrText[2], $arrText[3], $arrText[0]);
+			imagepng($image, $strCacheFolder.$strCacheName.'.png', 0);
+			imagedestroy($image);
+			echo file_get_contents($strCacheFolder.$strCacheName.'.png');
+			exit;
+		}
 
-        imagepng($image);
-        imagedestroy($image);
     }
 }
 
